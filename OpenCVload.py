61186a051199af996f0138ce01a2c0f26f8d6fc3
Murasaki-718 <111617052+Murasaki-718@users.noreply.github.com>
2022-10-18 22:09:10 +0900

    Add files via upload

---
+++ OpenCVload.py
@@ -0,0 +1,4 @@
+face_classifier = "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier(face_classifier)
+eye_classifier = "/usr/local/share/OpenCV/haarcascades/haarcascade_eye.xml"
+eye_cascade = cv2.CascadeClassifier(eye_classifier)
